83421a99922f843a94f86374f7c8217a8e9407e0
MatheuRezende <129076633+MatheuRezende@users.noreply.github.com>
2025-04-07 23:52:16 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,11 @@ import os
 
 app = Flask(__name__, static_folder='frontend', static_url_path='')
 
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
 
 
 @app.route('/img/<path:filename>')
